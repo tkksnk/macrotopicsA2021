6cf414cecc1d44546612e1e1f3c49a05c2e96525
Takeki Sunakawa <takeki.sunakawa@gmail.com>
2021-11-11 16:47:36 +0900

    ch3

--- CRRA.m
+++ CRRA.m
@@ -1,9 +1,9 @@
-%%file CRRA.m
-function util = CRRA(cons, gamma)
+function util = CRRA(cons,gamma)
 
-if gamma ~= 1
-%     util = cons.^(1-gamma)./(1-gamma);
+if gamma ~= 1;
     util = (cons.^(1-gamma)-1)./(1-gamma);
 else
     util = log(cons);
 end
+
+return;
